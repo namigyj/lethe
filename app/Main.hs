4ea036f81b33436f08c29703c8bded7b8a09b5f8
ngyj <ngyj@cinnabar.fr>
2018-06-06 09:37:12 +0200

    Read/write from FTP and make cmdHandler IO (instead of Either)

--- app/Main.hs
+++ app/Main.hs
@@ -7,55 +7,54 @@ import System.Exit
 import FtpIO
 import CmdLethe
 import qualified Lib as L
-
--- DELETE ME
-tmpTest = "/home/zerotsu/code/web/dfc.moe/data/links.json"
+import Network.FTP.Client (quit)
 
 main :: IO ()
-main = do someFunc
-      {-- md <- mode
+main = do h <- readConfig >>= ftpconn
+          md <- mode
           print md
-          rs <- readLinks tmpTest
-          execOrQuit (cmdHandler md) rs
-       --}
-  -- There must be a better way to do Error handling, but I have no idea ...
-execOrQuit :: Either (L.ResultSet -> L.ResultSet) (IO ()) -> L.ResultSet -> IO ()
-execOrQuit (Left f) rs = writeLinks tmpTest $ f rs
-execOrQuit (Right err) _ = err
+          rs <- readCurl
+          -- TODO : handle Result message (print sth ?)
+          cmdHandler md
+            >>= \f -> writeFtp h (f rs) -- keep warning for TODO
+            >>= print
+          quit h
+            >>= print
+
 
 -- cmdHandler should be :: Lethe -> (ResultSet -> ResultSet)
 -- that way it only gives back an operation, independant of the data we would apply on it
-cmdHandler :: Lethe -> Either (L.ResultSet -> L.ResultSet) (IO ())
-cmdHandler Link{method=Mod, uid=Nothing} = Right $ badArg "id of the link required"
-cmdHandler Link{method=Del, uid=Nothing} = Right $ badArg "id of the link required"
-cmdHandler Cat{method=Del} = Right $ badArg "forbidden to delete a category"
-cmdHandler Cat{method=Mod, catname=Nothing} = Right $ badArg "CATEGORY_NAME of the target is required"
+cmdHandler :: Lethe -> IO (L.ResultSet -> L.ResultSet)
+cmdHandler Refresh
+    = return $ L.computeAllUids
+cmdHandler Link{method=Mod, uid=Nothing} = badArg "id of the link required"
+cmdHandler Link{method=Del, uid=Nothing} =  badArg "id of the link required"
+cmdHandler Cat{method=Del} = badArg "forbidden to delete a category"
+cmdHandler Cat{method=Mod, catname=Nothing} = badArg "CATEGORY_NAME of the target is required"
 -- default catname
 cmdHandler ln@Link{method=Add, catname=Nothing}
     = cmdHandler ln{catname=Just "Articles"}
 cmdHandler Link{method=Add, title=t, url=u, catname=Just cn, tags=tg}
     = if any (=="") [t,u]
-      then Right $ badArg "title and url are required"
-      else Left $ L.appendItem cn (L.Item "" t u $ splitTags tg)
+      then badArg "title and url are required"
+      else return $ L.appendItem cn (L.Item "" t u $ splitTags tg)
 
 cmdHandler Link{method=Mod, uid=Just id', title=t, url=u, tags=tg}
-    = Left $ L.modItem id' (L.Item "" t u $ splitTags tg)
+    = return $ L.modItem id' (L.Item "" t u $ splitTags tg)
 
 cmdHandler Link{method=Del, uid=Just id'}
-    = Left $ L.deleteItem id'
+    = return $ L.deleteItem id'
 
 cmdHandler Cat{method=Add, name_=n, description=d}
     = if any (=="") [n,d]
-      then Right $ badArg "name and description are required"
-      else Left $ L.appendCategory (L.Category n d [])
+      then badArg "name and description are required"
+      else return $ L.appendCategory (L.Category n d [])
 
 cmdHandler Cat{method=Mod, catname=Just cn, name_=n, description=d}
-    = Left $ L.modCategory cn (L.Category n d [])
-cmdHandler Refresh
-    = Left $ L.computeAllUids
+    = return $ L.modCategory cn (L.Category n d [])
 
 
-badArg :: String -> IO ()
+badArg :: String -> IO (L.ResultSet -> L.ResultSet)
 badArg msg = die $ "[Error] Bad argument: " ++ msg
 
 -- Tags is a ','-separated list in a string
--- src/FtpIO.hs
+++ src/FtpIO.hs
@@ -1,3 +1,4 @@
+
 module FtpIO where
 
 import Lib
@@ -5,37 +6,56 @@ import Lib
 import           System.Exit
 import           System.IO()
 import           Data.Aeson
+import           Data.ByteString.Lazy.Char8  (pack, unpack)
+import           Data.List.Utils             (seqList)
+import qualified Network.Curl          as C
 import qualified Data.ByteString       as B  (readFile)
 import qualified Data.ByteString.Lazy  as BL
 import qualified System.Directory      as SD
-
+import qualified Network.FTP.Client    as NFC
+import Network.FTP.Client.Parser (FTPResult)
 
 testLocation :: FilePath
 testLocation = "/home/zerotsu/code/haskell/lethe/links.json"
 
-someFunc :: IO ()
-someFunc = readConfig print
+fileLocation :: String
+fileLocation = "www/data/links.json"
 
-writeLinks :: FilePath -> ResultSet -> IO ()
-writeLinks filepath rs = do BL.writeFile filepath . encode $ rs
 
-readLinks :: FilePath -> IO ResultSet
-readLinks filepath = do str <- B.readFile filepath
-                        return . getSet. BL.fromStrict $ str
+-- | Connect to the ftp server after reading config File
+ftpconn :: FtpParams -> IO NFC.FTPConnection
+ftpconn (FtpParams h u p) = do con <- NFC.easyConnectFTP h
+                               _ <- NFC.login con u (Just p) Nothing
+                               return con
+
+-- | get the file but with curl, because I can't get around the laziness
+-- | despite using seqList
+readCurl :: IO ResultSet
+readCurl = C.curlGetString "https://dfc.moe/data/links.json" []
+           >>= return . getSet . pack . snd
+
+-- | get the ResultSet from the JSONfile from the FTPConnection
+readFtp :: NFC.FTPConnection -> IO ResultSet
+readFtp h = do s <- NFC.getlines h fileLocation
+               return . getSet . pack . unlines $ seqList (fst s)
+
+-- | write the ResultSet to the file on the FtpConnection
+writeFtp :: NFC.FTPConnection -> ResultSet -> IO FTPResult
+writeFtp h rs = NFC.putlines h fileLocation . lines . unpack . encode  $ rs
+
 
 getSet :: BL.ByteString -> ResultSet
 getSet str = case (decode str) of
   Just x -> x
   Nothing -> ResultSet []
 
-data Conf = Conf { host :: String
-                 , user :: String
-                 , pass :: String
-                 } deriving (Show)
--- SD.getXdgDirectory SD.XdgConfig "lethe.conf" -> ~/.config/lethe.conf
--- SD.doesFileExist :: FilePath -> Bool
-mkConf :: [(String, String)] -> Conf
-mkConf ls = iter ls $ Conf "" "" ""
+data FtpParams = FtpParams { host :: String
+                           , user :: String
+                           , pass :: String
+                           } deriving (Show)
+
+mkParams :: [(String, String)] -> FtpParams
+mkParams ls = iter ls $ FtpParams "" "" ""
   where
     iter [] conf = conf
     iter ((k,v):xs) conf = iter xs cc
@@ -52,16 +72,16 @@ split2 s str = iter "" str
         iter acc (x:xs) | x == s    = (acc, xs)
                         | otherwise = iter (acc++[x]) xs
 
-readConfig :: (Conf -> IO ()) -> IO ()
-readConfig f = exitIfNotExist configFile
-             $ configFile >>= readFile >>= (f . mkConf . parse)
+readConfig :: IO FtpParams
+readConfig = do exitIfNotExist configFile
+                configFile >>= readFile >>= (return . mkParams . parse)
 
-exitIfNotExist :: IO FilePath -> IO () -> IO ()
-exitIfNotExist fpath next = do fp <- fpath
-                               exist <- fpath >>= SD.doesFileExist
-                               if not exist
-                                 then die $ "file " ++ fp ++ " not exist"
-                                 else next
+exitIfNotExist :: IO FilePath -> IO ()
+exitIfNotExist fpath = do fp <- fpath
+                          exist <- fpath >>= SD.doesFileExist
+                          if not exist
+                            then die $ "file " ++ fp ++ " does not exist"
+                            else return ()
 
 configFile :: IO FilePath
 configFile = SD.getXdgDirectory SD.XdgConfig "lethe.conf"
