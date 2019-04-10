fe71f2f44919956f807e6b6137e548e11dfe6490
ngyj <ngyj@cinnabar.fr>
2019-04-10 23:32:44 +0200

    WIP rewrite: Lib with lenses and run

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
+import Text.Read (readMaybe)
+import Data.Word (Word16)
+
 import System.Console.CmdArgs
+
+import Lib
 
 main :: IO ()
 main = print =<< mode
@@ -9,41 +15,41 @@ main = print =<< mode
 data Method = Add | Mod | Del
               deriving (Data, Typeable, Show, Eq)
 data Command
-  = Link { title::Maybe String, url::Maybe String, tags::Maybe String
-         , uid::Maybe String, method::Method, catname::String }
-  | Cat  { description::Maybe String, newname::Maybe String
-         , method::Method, catname::String }
+  = Link { title'::String, url'::String, tags'::String
+         , uid'::String, method'::Method, catname'::String }
+  | Cat  { description'::String, newname'::String
+         , method'::Method, catname'::String }
   | Refresh
   deriving (Data, Typeable, Show, Eq)
 
 link :: Command
 link = Link
-  { catname = "Articles" &= name "c"  &= typ "STR"
+  { catname' = "Articles" &= name "c"  &= typ "STR"
               &= help "category name to add the link to (default: Articles)"
-  , title = def &= name "t" &= typ "STR"
+  , title' = "" &= name "t" &= typ "STR"
             &= help "title of the item"
-  , url = def &= name "u" &= typ "STR"
+  , url' = "" &= name "u" &= typ "STR"
           &= help "url of the item"
-  , tags = def &= typ "LIST" &= name "g"
+  , tags' = def &= typ "LIST" &= name "g"
            &= help "comma (,) separated list of tags"
-  , method = enum
+  , method' = enum
         [ Add &= help "add a new item (default)"
         , Mod &= help "modify an existing item"
         , Del &= help "delete an existing item"
         ]
-  , uid = def &= args &= typ "UID"
+  , uid' = def &= args &= typ "UID"
   } &= help "add, modify or delete a single bookmark item" &= auto
 
 cat :: Command
 cat = Cat
-  { catname = def &= args &= typ "OLD_CATNAME"
-  , description = def &= name "d" &= typ "STR"
+  { catname' = def &= args &= typ "OLD_CATNAME"
+  , description' = def &= name "d" &= typ "STR"
                   &= help "new category short description"
-  , method = enum
+  , method' = enum
         [ Add &= help "Add a new category (default)"
         , Mod &= help "modify an existing category"
         ]
-  , newname = def &= typ "STR"
+  , newname' = def &= typ "STR"
               &= help "new category name"
   } &= help "Add or modify a category of bookmarks"
 
@@ -56,3 +62,22 @@ mode = cmdArgs $ modes [link, cat, refresh]
   &= program "lethe"
   &= summary ("lethe " ++ "v0.2" ++ "\nRecord it and never forgetti")
   &= helpArg [explicit, name "help", name "h"]
+
+run :: Command -> ResultSet -> Either String ResultSet
+run Refresh = pure . computeAllUids
+run Link{method',title',url',uid',tags',catname'} =
+  let tags = splitTags tags'
+      it = computeuid $ Item{_title=title',_url=url',_tags=tags,_uid=0}
+      uid :: Either String Word16
+      uid = case readMaybe ("0x"<>uid') of
+              Nothing -> Left ("Could not parse uid: " <> uid')
+              Just x -> Right x
+  in case method' of
+    Add -> pure . addItem catname' it
+    Mod -> pure . modItem it
+    Del -> \r -> flip delItem r <$> uid
+run Cat{method',catname',newname',description'} =
+  let it = Category {_name=newname',_description=description',_items=[]}
+  in case method' of
+    Add -> pure . addCategory it
+    Mod -> pure . modCategory catname' it
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
-module Lib where
+{-# LANGUAGE BangPatterns #-}
+module Lib ( ResultSet (..)
+           , Category (..)
+           , Item (..)
+           , computeAllUids
+           , computeuid
+           , computeuid'
+           , addItem
+           , modItem
+           , delItem
+           , addCategory
+           , modCategory
+           , splitTags
+           ) where
 
 import GHC.Word (Word16)
+import System.Console.CmdArgs (Data, Typeable)
 
 import Lens.Micro
 import Lens.Micro.TH (makeLenses)
@@ -9,18 +24,17 @@ import Lens.Micro.TH (makeLenses)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+-- | list of all categories
 newtype ResultSet = ResultSet { _cats :: [Category]
                               } deriving (Eq, Show)
 
 -- | category of bookmark links
-data Category = Category { _name :: !String         -- ^ name of the Category
-                         , _description :: !String  -- ^ description of it
+data Category = Category { _name :: !String        -- ^ name of the Category
+                         , _description :: !String -- ^ description of it
                          , _items :: [Item]        -- ^ collection of bookmark links
                          } deriving (Eq, Show)
 
--- | The bookmark link struct
--- | FIXME : The uid should become an Int once there's a consequent amount
--- |         of data (hex -> Int) applied during reading, I think
+-- | a bookmark item
 data Item = Item { _title :: !String  -- ^ The title/name of the bookmark link
                  , _url :: !String    -- ^ URL
                  , _tags :: ![String] -- ^ List of tags (for later being able to query)
@@ -31,22 +45,77 @@ makeLenses ''ResultSet
 makeLenses ''Category
 makeLenses ''Item
 
+-- | re-computes all the uids of Items in the entire ResultSet
+-- | FIXME : make sure they are unique
+computeAllUids :: ResultSet -> ResultSet
+computeAllUids = cats.each.items.each %~ computeuid
+
+-- | update an item's uid
+computeuid :: Item -> Item
+{-# WARNING computeuid "TODO" #-}
+computeuid = id
+
+-- | compute the uid of an item
+computeuid' :: Item -> Word16
+{-# WARNING computeuid' "TODO" #-}
+computeuid' = _uid
+
+-- | append a new category
+addCategory :: Category -> ResultSet -> ResultSet
+addCategory = (cats <>~) . pure
+
+-- | modify the category with the given name
+modCategory :: String -> Category -> ResultSet -> ResultSet
+modCategory cname cat = cats.each.filtered (\c -> _name c == cname) %~ (cat ??)
+
+-- | append a new Item to the given name's Category items
 addItem :: String -> Item -> ResultSet -> ResultSet
 addItem cname i = cats . singular each.filtered (\c -> _name c == cname) . items <>~ [i]
 
+-- | delete all items with given uid from all categories
+-- | FIXME fail and/or warn if two items have same uid
 delItem :: Word16 -> ResultSet -> ResultSet
-delItem uid = cats . each.items %~ filter (\i -> _uid i /= uid)
+delItem uid = cats.each.items %~ filter (\i -> _uid i /= uid)
 
--- FIXME better name
+-- | update the _first_ item with the same uid
+-- | FIXME fail and/or warn if two items have same uid
+modItem :: Item -> ResultSet -> ResultSet
+modItem it = cats.each . items.singular each. filtered (\i -> _uid i == _uid it) %~ (it ??)
+
+-- | FIXME : better name
+-- | instance for Item also recompute the uids
 class Default a where
+-- | if lhs is default return rhs
   (??) :: a -> a -> a
-
 instance Default [a] where
   [] ?? ys = ys
   xs ?? _  = xs
+instance Default Item where
+  i1 ?? i2 = computeuid $ Item{ _title = _title i1 ?? _title i2
+                              , _url   = _url i1 ?? _url i2
+                              , _tags  = _tags i1 ?? _tags i2
+                              , _uid   = 0
+                              }
+instance Default Category where
+  c1 ?? c2 = Category{ _name = _name c1 ?? _name c2
+                     , _description = _description c1 ?? _description c2
+                     , _items = _items c1 ?? _items c2
+                     }
 
-modItem :: Item -> ResultSet -> ResultSet
-modItem it = cats.each . items.singular each.filtered (\i -> _uid i == _uid it) %~ id
+-- | split on Char
+splitTags :: String -> [String]
+splitTags = splitOn ','
+
+-- | FIXME please rewrite me I'm shit.
+splitOn :: Char -> String -> [String]
+splitOn c = go [] ""
+  where
+    go !s "" "" = s
+    go s e ""  = s <> [reverse e]
+    go s e (c:xs) = case c of
+      ',' -> go (s <> [reverse e]) "" xs
+      ' ' -> go s ('_':e) xs
+      _   -> go s (c:e) xs
 
 -- tests
 x,y,z :: Item
